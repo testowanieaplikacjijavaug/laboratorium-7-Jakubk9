ef8bdb69db5197888251899abfc6ca3b688c23cf
Jakub Kisłowski <35227458+Jakubk9@users.noreply.github.com>
2020-03-30 10:49:32 +0200

    Added NoteTest

--- src/test/java/NoteTest.java
+++ src/test/java/NoteTest.java
@@ -1,10 +1,42 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class NoteTest {
 
+    public static Note note;
+
     @Test
-    public void alwaysPass(){
-        assertTrue(true);
+    public void getNameTest() {
+        note = Note.of("Kuba",4);
+        assertEquals(note.getName(), "Kuba");
+    }
+
+    @Test
+    public void getNoteTest() {
+        note = Note.of("Kuba",4);
+        assertEquals(note.getNote(),4);
+    }
+
+    @Test
+    public void givenNullNameTest() {
+        assertThatThrownBy(() -> {
+            Note.of(null, 4);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Imię ucznia nie może być null");
+    }
+
+    @Test
+    public void givenEmptyNameTest() {
+        assertThatThrownBy(() -> {
+            Note.of("", 4);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Imię ucznia nie może być puste");
+    }
+
+    @Test
+    public void givenOutOfRangeMarkTest() {
+        assertThatThrownBy(() -> {
+            Note.of("Kuba", 1);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Niewłaściwa ocena");
     }
 }
