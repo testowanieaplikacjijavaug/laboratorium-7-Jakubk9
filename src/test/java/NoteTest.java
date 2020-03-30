49e980ba524a1597145032c49b97fe3d18d2ed14
Jakub Kisłowski <35227458+Jakubk9@users.noreply.github.com>
2020-03-30 11:00:56 +0200

    refactor

--- src/test/java/NoteTest.java
+++ src/test/java/NoteTest.java
@@ -23,27 +23,31 @@ public class NoteTest {
     public void givenNullNameTest() {
         assertThatThrownBy(() -> {
             Note.of(null, 4);
-        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Imię ucznia nie może być null");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Imię ucznia nie może być null");
     }
 
     @Test
     public void givenEmptyNameTest() {
         assertThatThrownBy(() -> {
             Note.of("", 4);
-        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Imię ucznia nie może być puste");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Imię ucznia nie może być puste");
     }
 
     @Test
     public void givenOutOfRangeMarkTest() {
         assertThatThrownBy(() -> {
             Note.of("Kuba", 1);
-        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Niewłaściwa ocena");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Niewłaściwa ocena");
     }
 
     @Test
     public void givenOutOfRangeMarkAnotherArgumentTest() {
         assertThatThrownBy(() -> {
             Note.of("Kuba", 7);
-        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Niewłaściwa ocena");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Niewłaściwa ocena");
     }
 }
