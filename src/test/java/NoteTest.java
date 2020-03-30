3ef04aa9f123e7f7dcbf5de5a7367bc6358c1c90
Jakub Kisłowski <35227458+Jakubk9@users.noreply.github.com>
2020-03-30 10:16:30 +0200

    Added class to test

---
+++ src/test/java/NoteTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NoteTest {
+
+    @Test
+    public void alwaysPass(){
+        assertTrue(true);
+    }
+}
